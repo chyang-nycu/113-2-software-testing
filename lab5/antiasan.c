9e06cc64bf0f06c18e1d35b32f14899bd9616972
chyang <chyang.be10@nycu.edu.tw>
2025-04-30 03:01:40 +0800

    feat: lab5

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -2,5 +2,15 @@
 
 void antiasan(unsigned long addr)
 {
+    // 透過 extern 在函式裡隱式宣告 ASan 的 unpoison 介面
+    extern void __asan_unpoison_memory_region(void *addr, size_t size);
 
+    extern char gS[];
+    extern char gBadBuf[];
+
+    // 1) 先把 gS 本體 (0x18 bytes) + overflow (0x10 bytes) 一次 unpoison
+    __asan_unpoison_memory_region((void*)gS, 0x18 + 0x10);
+
+    // 2) 再把 gBadBuf 本體 (0x87 bytes) unpoison
+    __asan_unpoison_memory_region((void*)gBadBuf, 0x87);
 }
